5ed7d8d11cc0d349f9760526148a72b70d89ff0e
Gabriel Medina <gmedina.santos@gmail.com>
2018-08-25 13:18:39 -0400

    Add rack-cors and config the middleware

--- config/application.rb
+++ config/application.rb
@@ -18,16 +18,14 @@ Bundler.require(*Rails.groups)
 
 module Pub
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:8081'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
