af15421230c4302cebcd88fcdec03492efe0edee
Gabriel Medina <gmedina.santos@gmail.com>
2018-09-29 12:46:30 -0400

    Fixed to products and categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180901155906) do
+ActiveRecord::Schema.define(version: 20180910003352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "beers", force: :cascade do |t|
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_categories_on_slug", unique: true
+  end
+
+  create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
   end
 
   create_table "products", force: :cascade do |t|
@@ -27,6 +43,11 @@ ActiveRecord::Schema.define(version: 20180901155906) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["slug"], name: "index_products_on_slug", unique: true
   end
 
+  add_foreign_key "products", "categories"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,54 @@
 # Seeds
 
-Beer.create({
+Category.destroy_all
+
+category = Category.create({
+  name: 'Cervejas',
+  description: 'Conheça nossas cervejas artesanais'
+})
+
+p "Created #{Category.count} Categories!"
+
+Product.destroy_all
+
+Product.create({
   name: 'Heineken',
   price: 6.0,
   quantity: 202,
-  image: 'http://localhost:3000/uploads/heineken.jpg'
+  image: 'http://localhost:3000/uploads/heineken.jpg',
+  category: category
 })
 
-Beer.create({
+Product.create({
   name: 'Budweiser',
   price: 5.5,
   quantity: 121,
-  image: 'http://localhost:3000/uploads/budweiser.jpg'
+  image: 'http://localhost:3000/uploads/budweiser.jpg',
+  category: category
 })
 
-Beer.create({
+Product.create({
   name: 'Eisenbahn',
   price: 5.0,
   quantity: 2,
-  image: 'http://localhost:3000/uploads/eisenbahn.jpg'
+  image: 'http://localhost:3000/uploads/eisenbahn.jpg',
+  category: category
 })
 
-Beer.create({
+Product.create({
   name: 'Antarctica Original',
   price: 6.0,
   quantity: 24,
-  image: 'http://localhost:3000/uploads/antarctica-original.jpg'
+  image: 'http://localhost:3000/uploads/antarctica-original.jpg',
+  category: category
 })
 
-Beer.create({
+Product.create({
   name: 'Bohemia',
   price: 7.0,
   quantity: 139,
-  image: 'http://localhost:3000/uploads/bohemia.jpg'
+  image: 'http://localhost:3000/uploads/bohemia.jpg',
+  category: category
 })
+
+p "Created #{Product.count} Products!"
